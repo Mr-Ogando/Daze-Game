0927717cef9a14a079c981d7f1598ffb645fa98b
Luis E Ogando <elgreatness23@gmail.com>
2022-06-03 00:43:39 -0400

    tryting to create some shapes (rectangle)  before start doing the daze game

--- index.js
+++ index.js
@@ -5,3 +5,32 @@
 // Runner makes the World and Engine work together
 // Todo Render
 const {World, Render, Runner, Bodies, Engine}=Matter;
+
+const cells = 3;
+const width = 600;
+const height = 600;
+
+const unitLength = width / cells;
+
+const engine = Engine.create();
+const { world } = engine;
+const render = Render.create({
+  element: document.body,
+  engine: engine,
+  options: {
+    wireframes: true,
+    width,
+    height
+  }
+});
+Render.run(render);
+Runner.run(Runner.create(), engine);
+
+// Walls
+const walls = [
+  Bodies.rectangle(width / 2, 0, width, 40, { isStatic: true }),
+  Bodies.rectangle(width / 2, height, width, 40, { isStatic: true }),
+  Bodies.rectangle(0, height / 2, 40, height, { isStatic: true }),
+  Bodies.rectangle(width, height / 2, 40, height, { isStatic: true })
+];
+World.add(world, walls);
