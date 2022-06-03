c794f248cd146b3340399096ecce3cf5d2edf87d
Luis E Ogando <elgreatness23@gmail.com>
2022-06-03 00:39:31 -0400

    adding new project Daze for practicing JS and github

---
+++ index.js
@@ -0,0 +1,7 @@
+// ? using disctruting getting the objects from the Matter.js libary 
+// ! world= All the shapes that are on out app.
+//* Engine is an object which contains diffrent functions
+//* Will read the current state of the world and make changesn
+// Runner makes the World and Engine work together
+// Todo Render
+const {World, Render, Runner, Bodies, Engine}=Matter;
